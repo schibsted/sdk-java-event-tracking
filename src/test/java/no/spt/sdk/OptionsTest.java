c2b485d97898ef290655c7fa3dec4d7fd86ecb6d
Oskar Otterskog <oskarotterskog@gmail.com>
2015-02-17 08:45:09 +0100

    Test for Options

---
+++ src/test/java/no/spt/sdk/OptionsTest.java
@@ -0,0 +1,26 @@
+package no.spt.sdk;
+
+import org.junit.Test;
+
+public class OptionsTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetDataCollectorUrlEmptyString() throws Exception {
+        Options options = new Options("", Defaults.MAX_QUEUE_SIZE, Defaults.TIMEOUT, Defaults.RETRIES);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetMaxQueueSizeToNegativeValue() throws Exception {
+        Options options = new Options(Defaults.DATA_COLLECTOR_URL, -1, Defaults.TIMEOUT, Defaults.RETRIES);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetTimeoutToNegativeValue() throws Exception {
+        Options options = new Options(Defaults.DATA_COLLECTOR_URL, Defaults.MAX_QUEUE_SIZE, -1, Defaults.RETRIES);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetRetriesToNegativeValue() throws Exception {
+        Options options = new Options(Defaults.DATA_COLLECTOR_URL, Defaults.MAX_QUEUE_SIZE, Defaults.TIMEOUT, -1);
+    }
+}
