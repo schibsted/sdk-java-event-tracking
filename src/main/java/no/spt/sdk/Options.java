b2ffe81910551f1726a6fdf5191628648196098c
Oskar Otterskog <oskarotterskog@gmail.com>
2015-02-25 15:37:32 +0100

    Options should be immutable

--- src/main/java/no/spt/sdk/Options.java
+++ src/main/java/no/spt/sdk/Options.java
@@ -5,11 +5,11 @@ package no.spt.sdk;
  */
 public class Options {
 
-    private String dataCollectorUrl;
-    private String anonymousIdUrl;
-    private int maxQueueSize;
-    private int timeout;
-    private int retries;
+    private final String dataCollectorUrl;
+    private final String anonymousIdUrl;
+    private final int maxQueueSize;
+    private final int timeout;
+    private final int retries;
 
     /**
      * Constructs an Option with the provided settings
@@ -21,92 +21,87 @@ public class Options {
      * @param retries          the amount of times to retry the request
      */
     public Options(String dataCollectorUrl, String anonymousIdUrl, int maxQueueSize, int timeout, int retries) {
-        setDataCollectorUrl(dataCollectorUrl);
-        setAnonymousIdUrl(anonymousIdUrl);
-        setMaxQueueSize(maxQueueSize);
-        setTimeout(timeout);
-        setRetries(retries);
+        this.dataCollectorUrl = validateDataCollectorUrl(dataCollectorUrl);
+        this.anonymousIdUrl = validateAnonymousIdUrl(anonymousIdUrl);
+        this.maxQueueSize =validateMaxQueueSize(maxQueueSize);
+        this.timeout = validateTimeout(timeout);
+        this.retries = validateRetries(retries);
     }
 
+    /**
+     * Gets the data collector endpoint
+     * @return The data collector endpoint
+     */
     public String getDataCollectorUrl() {
         return dataCollectorUrl;
     }
 
-    /**
-     * Sets the data collector endpoint
-     *
-     * @param dataCollectorUrl
-     */
-    public void setDataCollectorUrl(String dataCollectorUrl) {
+    private String validateDataCollectorUrl(String dataCollectorUrl) {
         if(dataCollectorUrl == null || dataCollectorUrl.equals("")) {
             throw new IllegalArgumentException("Data-collector-sdk#options#dataCollectorUrl must be a valid url.");
         }
-        this.dataCollectorUrl = dataCollectorUrl;
+        return dataCollectorUrl;
     }
 
+    /**
+     * Gets the anonymous identity service endpoint
+     * @return The anonymous identity service endpoint
+     */
     public String getAnonymousIdUrl() {
         return anonymousIdUrl;
     }
 
-    /**
-     * Sets the anonymous identity service endpoint
-     *
-     * @param anonymousIdUrl
-     */
-    public void setAnonymousIdUrl(String anonymousIdUrl) {
+    private String validateAnonymousIdUrl(String anonymousIdUrl) {
         if(anonymousIdUrl == null || anonymousIdUrl.equals("")) {
             throw new IllegalArgumentException("Data-collector-sdk#options#anonymousIdUrl must be a valid url.");
         }
-        this.anonymousIdUrl = anonymousIdUrl;
+        return anonymousIdUrl;
     }
 
+    /**
+     * Gets the maximum size of the activity queue waiting to be sent to the data collector.
+     * If the queue has reached the maximum size it will no longer accept new activities and those will be dropped.
+     * @return The maximum size of the activity queue waiting to be sent to the data collector
+     */
     public int getMaxQueueSize() {
         return maxQueueSize;
     }
 
-    /**
-     * Sets the maximum size of the activity queue waiting to be sent to the data collector.
-     * If the queue has reached the maximum size it will no longer accept new activities and those will be dropped.
-     *
-     * @param maxQueueSize
-     */
-    public void setMaxQueueSize(int maxQueueSize) {
+    private int validateMaxQueueSize(int maxQueueSize) {
         if(maxQueueSize < 1) {
             throw new IllegalArgumentException("Data-collector-sdk#options#maxQueueSize must be greater than 0.");
         }
-        this.maxQueueSize = maxQueueSize;
+        return maxQueueSize;
     }
 
+    /**
+     * Gets the amount of milliseconds before an HTTP request is marked as timed out
+     * @return The amount of milliseconds before an HTTP request is marked as timed out
+     */
     public int getTimeout() {
         return timeout;
     }
 
-    /**
-     * Sets the amount of milliseconds before a request is marked as timed out
-     *
-     * @param timeout
-     */
-    public void setTimeout(int timeout) {
+    private int validateTimeout(int timeout) {
         if(timeout < 500) {
             throw new IllegalArgumentException("Data-collector-sdk#options#timeout must be at least 500 milliseconds.");
         }
-        this.timeout = timeout;
+        return timeout;
     }
 
+    /**
+     * Gets the amount of times to retry an HTTP request
+     * @return The amount of times to retry an HTTP request
+     */
     public int getRetries() {
         return retries;
     }
 
-    /**
-     * Sets the amount of times to retry the request
-     *
-     * @param retries
-     */
-    public void setRetries(int retries) {
+    private int validateRetries(int retries) {
         if(retries < 0) {
             throw new IllegalArgumentException("Data-collector-sdk#options#retries must be greater or equal to 0.");
         }
-        this.retries = retries;
+        return retries;
     }
 
 }
