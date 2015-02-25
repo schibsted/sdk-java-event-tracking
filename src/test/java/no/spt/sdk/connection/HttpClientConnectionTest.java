1e3a32bb6ee9f4f3f9e6355086d15aaa5d936c90
Oskar Otterskog <oskarotterskog@gmail.com>
2015-02-25 16:17:51 +0100

    Improved HttpClientConnection tests

--- src/test/java/no/spt/sdk/connection/HttpClientConnectionTest.java
+++ src/test/java/no/spt/sdk/connection/HttpClientConnectionTest.java
@@ -4,6 +4,7 @@ import no.spt.sdk.Options;
 import no.spt.sdk.TestData;
 import no.spt.sdk.client.DataTrackingPostRequest;
 import no.spt.sdk.client.DataTrackingResponse;
+import no.spt.sdk.exceptions.DataTrackingException;
 import no.spt.sdk.models.Activity;
 import no.spt.sdk.serializers.ASJsonConverter;
 import no.spt.sdk.serializers.GsonASJsonConverter;
@@ -11,7 +12,6 @@ import org.apache.http.HttpStatus;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,12 +20,13 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class HttpClientConnectionTest {
@@ -43,14 +44,9 @@ public class HttpClientConnectionTest {
         connection = new HttpClientConnection(options, httpClient);
     }
 
-    @After
-    public void tearDown() throws Exception {
-        connection.close();
-    }
-
     @Test
     public void testSendWithOkResponse() throws Exception {
-        DataTrackingResponse mockResp = new DataTrackingResponse(HttpStatus.SC_OK, null, "OK");
+        DataTrackingResponse mockResp = new DataTrackingResponse(HttpStatus.SC_OK, getHeaders(), "OK");
         when(httpClient.execute(any(HttpPost.class), any(ResponseHandler.class))).thenReturn(mockResp);
         DataTrackingResponse resp = connection.send(asRequest(Arrays.asList(TestData.getTestActivity())));
         assertEquals(mockResp, resp);
@@ -59,20 +55,20 @@ public class HttpClientConnectionTest {
     @Test
     public void testSendWithSomeValidationFailedResponse() throws Exception {
         when(httpClient.execute(any(HttpPost.class), any(ResponseHandler.class))).thenReturn(new DataTrackingResponse
-                (HttpStatus.SC_MULTI_STATUS, null, "ValidationFailedForSomeActivity"));
+                (HttpStatus.SC_MULTI_STATUS, getHeaders(), "ValidationFailedForSomeActivity"));
         DataTrackingResponse resp = connection.send(asRequest(Arrays.asList(TestData.getTestActivity())));
         assertEquals(HttpStatus.SC_MULTI_STATUS, resp.getResponseCode());
-        assertNull(resp.getHeaders());
+        assertEquals(getHeaders(), resp.getHeaders());
         assertEquals("ValidationFailedForSomeActivity", resp.getRawBody());
     }
 
     @Test
     public void testSendWithBadRequestResponse() throws Exception {
         when(httpClient.execute(any(HttpPost.class), any(ResponseHandler.class))).thenReturn(new DataTrackingResponse
-                (HttpStatus.SC_BAD_REQUEST, null, "BadRequest"));
+                (HttpStatus.SC_BAD_REQUEST, getHeaders(), "BadRequest"));
         DataTrackingResponse resp = connection.send(asRequest(Arrays.asList(TestData.getTestActivity())));
         assertEquals(HttpStatus.SC_BAD_REQUEST, resp.getResponseCode());
-        assertNull(resp.getHeaders());
+        assertEquals(getHeaders(), resp.getHeaders());
         assertEquals("BadRequest", resp.getRawBody());
 
     }
@@ -80,19 +76,33 @@ public class HttpClientConnectionTest {
     @Test
     public void testSendUnexpectedResponse() throws Exception {
         when(httpClient.execute(any(HttpPost.class), any(ResponseHandler.class))).thenReturn(new DataTrackingResponse
-                (HttpStatus.SC_NOT_IMPLEMENTED, null, "NotImplementedExample"));
+                (HttpStatus.SC_NOT_IMPLEMENTED, getHeaders(), "NotImplementedExample"));
         DataTrackingResponse resp = connection.send(asRequest(Arrays.asList(TestData.getTestActivity())));
         assertEquals(HttpStatus.SC_NOT_IMPLEMENTED, resp.getResponseCode());
-        assertNull(resp.getHeaders());
+        assertEquals(getHeaders(), resp.getHeaders());
         assertEquals("NotImplementedExample", resp.getRawBody());
     }
 
     @Test
     public void testClose() throws Exception {
         connection.close();
+        verify(httpClient, times(1)).close();
+    }
+
+    @Test(expected = DataTrackingException.class)
+    public void testCloseThrowsIOE() throws Exception {
+        doThrow(new IOException()).when(httpClient).close();
+        connection.close();
     }
 
     private DataTrackingPostRequest asRequest(List<Activity> activities) throws IOException {
-        return new DataTrackingPostRequest(options.getDataCollectorUrl(), null, jsonConverter.serialize(activities));
+        return new DataTrackingPostRequest(options.getDataCollectorUrl(), getHeaders(), jsonConverter.serialize(activities));
+    }
+
+    private static Map<String, String> getHeaders() {
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("key1", "value1");
+        headers.put("key2", "value2");
+        return headers;
     }
 }
