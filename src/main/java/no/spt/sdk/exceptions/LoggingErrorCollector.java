6a2d30c51e507edac6f6c74f4fb0d0e20f7264e1
Oskar Otterskog <oskarotterskog@gmail.com>
2015-03-02 14:24:15 +0100

    No need to save errors when logging

--- src/main/java/no/spt/sdk/exceptions/LoggingErrorCollector.java
+++ src/main/java/no/spt/sdk/exceptions/LoggingErrorCollector.java
@@ -1,6 +1,5 @@
 package no.spt.sdk.exceptions;
 
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -9,28 +8,21 @@ import java.util.logging.Logger;
  */
 public class LoggingErrorCollector implements ErrorCollector {
 
-    private static final int MAX_ERROR_QUEUE_SIZE = 10000;
-
     private static final Logger logger =
             Logger.getLogger(LoggingErrorCollector.class.getName());
 
     private Level logLevel;
-    private LinkedBlockingQueue<DataTrackingException> errorQueue;
 
     public LoggingErrorCollector() {
         this(Level.INFO);
     }
 
     public LoggingErrorCollector(Level logLevel) {
-        this.errorQueue = new LinkedBlockingQueue<DataTrackingException>();
         this.logLevel = logLevel;
     }
 
     @Override
     public void collect(DataTrackingException e) {
-        if(errorQueue.size() < MAX_ERROR_QUEUE_SIZE) {
-            errorQueue.add(e);
-        }
         logger.log(logLevel, e.getMessage());
         if(e.getResponseCode() != null) {
             logger.log(logLevel, String.format("Response code from data collector was: %s", e.getResponseCode()));
