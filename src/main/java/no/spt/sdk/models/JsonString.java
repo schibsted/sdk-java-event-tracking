dd77f8edc2b32e2079a1338ec7e80d8edae5c805
Oskar Otterskog <oskarotterskog@gmail.com>
2015-03-10 13:20:56 +0100

    Added class that represents a json String

---
+++ src/main/java/no/spt/sdk/models/JsonString.java
@@ -0,0 +1,14 @@
+package no.spt.sdk.models;
+
+public class JsonString {
+
+    private final String json;
+
+    public JsonString(String json) {
+        this.json = json;
+    }
+
+    public String getJson() {
+        return json;
+    }
+}
