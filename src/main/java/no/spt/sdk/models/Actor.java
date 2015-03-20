2f6bc070f49a7ce35a51086d4d1332cf357130dc
Oskar Otterskog <oskarotterskog@gmail.com>
2015-03-20 08:35:20 +0100

    Added constructors for creating creating Actor builders from TrackingIdentity

--- src/main/java/no/spt/sdk/models/Actor.java
+++ src/main/java/no/spt/sdk/models/Actor.java
@@ -17,9 +17,37 @@ public class Actor extends ASObject {
             super(type, id);
         }
 
+        public Builder(TrackingIdentity trackingId, ASObject actor) {
+            super(actor.getMap());
+            type("Person");
+            id(toIriVisitorId(trackingId.getVisitorId()));
+            set("spt:sessionId", toIriSessionId(trackingId.getSessionId()));
+            set("spt:environmentId", toIriEnvironmentId(trackingId.getEnvironmentId()));
+            set("spt:userId", trackingId.getUserId().isEmpty() ? null : trackingId.getUserId());
+        }
+
+        public Builder(TrackingIdentity trackingId) {
+            super("Person", toIriVisitorId(trackingId.getVisitorId()));
+            set("spt:sessionId", toIriSessionId(trackingId.getSessionId()));
+            set("spt:environmentId", toIriEnvironmentId(trackingId.getEnvironmentId()));
+            set("spt:userId", trackingId.getUserId().isEmpty() ? null : trackingId.getUserId());
+        }
+
         public Actor build() {
             return new Actor(this);
         }
+    }
+
+    private static String toIriVisitorId(String visitorId) {
+        return !visitorId.isEmpty() ? "urn:spid.no:person:" + visitorId : null;
+    }
+
+    private static String toIriEnvironmentId(String environmentId) {
+        return !environmentId.isEmpty() ? "urn:spid.no:environment:" + environmentId : null;
+    }
+
+    private static String toIriSessionId(String sessionId) {
+        return !sessionId.isEmpty() ? "urn:spid.no:session:" + sessionId : null;
     }
 
 }
